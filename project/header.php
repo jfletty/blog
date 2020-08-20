9a8bae8fa4a62d30cf5af657e68e390b52a1f70b
Josh Fletcher <fletcher.1996@hotmail.com>
2020-08-20 21:38:26 +1200

    FWFEED-Get post content displaying

--- project/header.php
+++ project/header.php
@@ -1,32 +1,42 @@
 <?php
-    class Environment
-    {
-        const Production = 'Production';
-        const Development = 'Development';
-    }
-    
-    $environment = $_SERVER['SERVER_NAME'] == "localhost" ? Environment::Development : Environment::Production;
-    $appsettings = $environment == Environment::Production ? "./appsettings.json" : "./appsettings.development.json";
+  class Environment
+  {
+    const Production = 'Production';
+    const Development = 'Development';
+  }
+  
+  $environment = $_SERVER['SERVER_NAME'] == "localhost" ? Environment::Development : Environment::Production;
+  $appsettings = $environment == Environment::Production ? "./appsettings.json" : "./appsettings.development.json";
 
-    $settings = json_decode(file_get_contents($appsettings), TRUE);
+  $settings = json_decode(file_get_contents($appsettings), TRUE);
 ?>
 
 <html>
+
 <body>
-<div class="container">
-<!-- Bootstrap- Start -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<!-- Bootstrap- End -->
+  <!-- Bootstrap- Start -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 
-<!-- Navbar- Start -->
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-      <img src="<?php echo $settings["SiteIdentity"]["Logo"] ?>" height="100" class="d-inline-block align-top" alt="">
-      <?php echo $settings["SiteIdentity"]["Name"] ?>
-    </a>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <!-- Bootstrap- End -->
+
+  <!-- Navbar- Start -->
+  <nav class="navbar navbar-expand-sm bg-<?php echo $settings["SiteIdentity"]["Theme"] ?> navbar-<?php echo $settings["SiteIdentity"]["Theme"] ?>">
+    <div class="container">
+      <!-- Brand/logo -->
+      <a class="navbar-brand" href="">
+        <?php echo $settings["SiteIdentity"]["Name"] ?>
+      </a>
+
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
   </nav>
+  </div>
   <!-- Navbar- End -->
--- project/index.php
+++ project/index.php
@@ -1,6 +1,42 @@
+<?php 
+        include "header.php";
+        $filesDir = "./posts";
+        $files = scan_dir($filesDir);
+        
 
-    <?php include "header.php"?>
+        $filesContent = [];
+        
+        foreach($files as &$item){
+            if($item !== "." && $item !== ".."){
+                $filesContent[$item] = file_get_contents($filesDir . '/' . $item);
+            }
+        }
+        
 
-        Content here
+        function scan_dir($dir) {
+            $ignored = array('.', '..', '.svn', '.htaccess');
+        
+            $files = array();    
+            foreach (scandir($dir) as $file) {
+                if (in_array($file, $ignored)) continue;
+                $files[$file] = filemtime($dir . '/' . $file);
+            }
+        
+            arsort($files);
+            $files = array_keys($files);
+        
+            return ($files) ? $files : false;
+        }
+    ?>
+  <div class="container">
 
-    <?php include "footer.php"?>
+<?php include "footer.php"?>
+
+<?php foreach($files as &$item){ ?>
+<div class="col-lg-12">
+    <h2><?php echo $item ?></h2>
+    <p><?php echo $filesContent[$item] ?></p>
+</div>
+<?php } ?>
+
+</div>
